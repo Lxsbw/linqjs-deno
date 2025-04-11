588f8519d12620ac3f327d8c656b95a4d19df0a7
zhixiong <lxsbw@outlook.com>
2025-04-11 22:54:50 +0800

    update test

---
+++ test/groupby_Many.js
@@ -0,0 +1,40 @@
+var data, result;
+
+import { Linq } from '../index.js';
+
+data = {
+  ZWMS_WERKS: '1000',
+  ZWMS_MATNR: '02312SCU-NFA01',
+  ZWMS_KCLX: '02',
+  ZWMS_KCSL: 7,
+  ZWMS_ZYSL: 2,
+  ZWMS_LGORT: 'BJ41',
+  ZWMS_NUMBE: 'Z2000156365',
+  ZWMS_CONTAC: '1Y01032503120R',
+  ZWMS_KCBJ: 'Z',
+};
+
+let list = [];
+for (let index = 0; index < 200000; index++) {
+  let ent = JSON.parse(JSON.stringify(data));
+  ent.id = (index + 1).toString();
+  list.push(ent);
+}
+
+// console.log('list:', list);
+// console.log('list:', list.length);
+
+
+// 分组
+
+console.time('groupBy');
+
+result = new Linq(list).groupBy(el => el.id);
+// result = new Linq(list).groupByMini(el => el.id);
+
+console.log('result:', result);
+
+console.log('list:', list.length);
+console.log('result:', result.length);
+
+console.timeEnd('groupBy');
