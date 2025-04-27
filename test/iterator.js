3823f122fc18fbc0ea8665cf8da350c4c9ca4eee
zhixiong <lxsbw@outlook.com>
2025-04-27 18:23:38 +0800

    update test

---
+++ test/iterator.js
@@ -0,0 +1,37 @@
+import { Linq } from '../index.js';
+
+const pets = new Linq([
+  { Age: 10, Name: 'Barley' },
+  { Age: 4, Name: 'Boots' },
+  { Age: 6, Name: 'Bissy' },
+]);
+
+console.log('=============================== for ... from ========================');
+for (const pet of pets) {
+  console.log('pet:', pet);
+}
+
+console.log();
+console.log('=============================== [...pets] ========================');
+const petsCopy = [...pets];
+for (const pet of petsCopy) {
+  console.log('petsCopy:', pet);
+}
+
+console.log();
+console.log('=============================== Array.from() ========================');
+const petsFrom = Array.from(pets);
+for (const pet of petsFrom) {
+  console.log('petsFrom:', pet);
+}
+
+console.log();
+console.log('=============================== new Set() ========================');
+const petsSet = new Set(pets);
+console.log('petsSet:', petsSet);
+
+
+console.log();
+const petsss = new Linq([]);
+console.log(petsss.toString() === '[object List]');
+console.log(`${petsss}` === '[object List]');
